bf7b504693be8e2595c7267ed83f7cfe0a2161bb
taidmhe150076 <taidmhe150076@fpt.edu.vn>
2024-03-25 01:08:37 +0700

    commit

--- PRN231_JollyOnlineShopping/JollyAPI/Models/Entity/Address.cs
+++ PRN231_JollyOnlineShopping/JollyAPI/Models/Entity/Address.cs
@@ -6,12 +6,12 @@ namespace JollyAPI.Models.Entity
     public partial class Address
     {
         public int Id { get; set; }
-        public string? City { get; set; }
-        public string? District { get; set; }
-        public string? Ward { get; set; }
-        public string? Street { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Ward { get; set; }
+        public string Street { get; set; }
         public int? UserId { get; set; }
 
-        public virtual User? User { get; set; }
+        public virtual User User { get; set; }
     }
 }
